50ce6bb03569847379e0f61621d4d6699a5d99d4
rusdevops <rusdevops@gmail.com>
2018-11-26 00:19:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,26 +3,29 @@ set -e
 #sudo apt-add-repository -y "ppa:ubuntu-toolchain-r/test" && sudo apt update
 #sudo add-apt-repository -y ppa:neovim-ppa/stable && apt update
 
-sudo apt install -y wget gconf2
-
+apt install -y wget
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 
-sudo apt-add-repository "deb http://apt.llvm.org/$(lsb_release -sc)/ llvm-toolchain-$(lsb_release -sc)-7 main"
-sudo apt install -y libcurl4-openssl-dev curl \
+apt-add-repository "deb http://apt.llvm.org/$(lsb_release -sc)/ llvm-toolchain-$(lsb_release -sc)-7 main"
+apt install -y libcurl4-openssl-dev curl \
      g++-7 lcov gcovr cmake cmake-data clang-7
 
-sudo apt -y install curl git git-extras zsh astyle python3-dev \
-     neovim silversearcher-ag python-pip \
-     tmux exuberant-ctags valgrind gdb
+apt install -y curl git git-extras zsh astyle python3-dev \
+    neovim silversearcher-ag python-pip \
+    tmux exuberant-ctags valgrind gdb gconf2
 
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-     libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
-     xz-utils tk-dev libffi-dev liblzma-dev
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
+    xz-utils tk-dev libffi-dev liblzma-dev
 
 git clone git://github.com/rkitover/vimpager
-cd vimpager && sudo make install && cd .. && rm -rf vimpager
+cd vimpager && make install && cd .. && rm -rf vimpager
 
 if [ ! -d "/usr/local/PathPicker" ]; then
-  sudo git clone https://github.com/facebook/PathPicker.git /usr/local/PathPicker
-  sudo ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
+  git clone https://github.com/facebook/PathPicker.git /usr/local/PathPicker
+  ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
 fi
+
+ln -s /usr/bin/clang-7 /usr/bin/clang
+ln -s /usr/bin/clang++-7 /usr/bin/clang++
+ln -s /usr/bin/clang-cpp-7 /usr/bin/clang-cpp
