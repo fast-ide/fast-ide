724b2382f1864f0dbd241f86ee9518b3446a0e20
rusdevops <rusdevops@gmail.com>
2018-11-26 00:13:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,12 +19,21 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
      libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
      xz-utils tk-dev libffi-dev liblzma-dev
 
+git clone git://github.com/rkitover/vimpager
+cd vimpager && sudo make install && cd .. && rm -rf vimpager
+
+if [ ! -d "/usr/local/PathPicker" ]; then
+  sudo git clone https://github.com/facebook/PathPicker.git /usr/local/PathPicker
+  sudo ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
+fi
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install v8.12.0
 nvm use v8.12.0
 nvm install-latest-npm
+npm install -g jscpd
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | zsh
 export PATH="$HOME/.pyenv/bin:$PATH"
@@ -34,26 +43,19 @@ eval "$(pyenv virtualenv-init -)"
 PYTHON_CONFIGURE_OPTS='--enable-shared' pyenv install --skip-existing 3.5.3
 pyenv shell 3.5.3
 
-pip3 install cpplint && npm install -g jscpd
-
-git clone git://github.com/rkitover/vimpager
-cd vimpager && sudo make install && cd .. && rm -rf vimpager
-
+pip3 install cpplint
 pip3 install komodo-python3-dbgp pynvim
 pip2 install pynvim
 
 rm -rf $HOME/.oh-my-zsh
 curl -L http://install.ohmyz.sh | sh || true
-
-if [ ! -d "/usr/local/PathPicker" ]; then
-  sudo git clone https://github.com/facebook/PathPicker.git /usr/local/PathPicker
-  sudo ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
-fi
-
 mkdir -p $HOME/.zsh
 curl -L git.io/antigen > $HOME/.zsh/antigen.zsh
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d "$HOME/.zsh/vimman" ]; then
+  git clone https://github.com/yonchu/vimman.git $HOME/.zsh/vimman
+fi
 
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.nvim/backup
@@ -73,9 +75,7 @@ nvim +PlugInstall +qall
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
-if [ ! -d "$HOME/.zsh/vimman" ]; then
-  git clone https://github.com/yonchu/vimman.git $HOME/.zsh/vimman
-fi
+
 zsh --rcs $HOME/.zshrc || true
 
 uuids=$(gsettings get org.gnome.Terminal.ProfilesList list | \
@@ -87,6 +87,6 @@ cd gnome-terminal-colors-solarized
 cd .. && rm -rf gnome-terminal-colors-solarized
 ./one-dark.sh
 
-sudo apt install -y docker.io
-sudo chsh -s /bin/zsh $USER
-sudo usermod -aG docker $USER
+# sudo apt install -y docker.io
+# sudo chsh -s /bin/zsh $USER
+# sudo usermod -aG docker $USER
