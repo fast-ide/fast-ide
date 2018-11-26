7f47927ccd404a5ae884931a581f0a9bcff4a061
rusdevops <rusdevops@gmail.com>
2018-11-26 08:04:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 # apt-add-repository -y "ppa:ubuntu-toolchain-r/test" && apt update
 # add-apt-repository -y ppa:neovim-ppa/stable && apt update
 
-apt install -y gnome-shell ubuntu-gnome-desktop
+apt update && apt install -y gnome-shell ubuntu-gnome-desktop
 apt install -y wget
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 
