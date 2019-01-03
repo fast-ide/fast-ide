b57e24d9aebd3990b7e4cd47f60e4f02b47f1604
rusdevops <rusdevops@gmail.com>
2019-01-03 13:58:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ apt install -y libcurl4-openssl-dev curl \
 
 apt install -y curl git git-extras zsh astyle python3-dev \
     neovim silversearcher-ag python-pip \
-    tmux exuberant-ctags valgrind gdb gconf2 docker.io
+    tmux exuberant-ctags valgrind gdb gconf2 docker.io \
+    graphviz doxygen pandoc
 
 apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
