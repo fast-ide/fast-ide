03322da4066d6fd72424fc9992c44e141827903a
rusdevops <rusdevops@gmail.com>
2019-02-11 10:06:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,8 @@ apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
     xz-utils tk-dev libffi-dev liblzma-dev
 
+apt install -y nfs-common cifs-utils
+
 git clone git://github.com/rkitover/vimpager
 cd vimpager && make install && cd .. && rm -rf vimpager
 
