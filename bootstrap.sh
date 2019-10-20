4c307b204e48a622c4086d278a6e4458bcc7aa27
rusdevops <rusdevops@gmail.com>
2019-10-20 12:45:38 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,115 +1,120 @@
 set -e
 set -x
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | zsh
+# Install brew
+
+git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+mkdir ~/.linuxbrew/bin
+ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+eval $(~/.linuxbrew/bin/brew shellenv)
+~/.linuxbrew/bin/brew shellenv
+
+# Install brew packages
+
+brew install zsh
+brew install pyenv
+brew install nvm
+
+brew install neovim
+brew install tmux
+
+brew install fpp
+brew install ag
+brew install fzf
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+
+brew install git-extras
+brew install tig
+
+brew install htop
+brew install catimg
+brew install highlight
+
+brew install grc
+brew install jid
+
+brew install httpie
+brew install sqlite3
+
+
+# Install nodejs
+
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$HOME/.linuxbrew/opt/nvm/nvm.sh" ] && . "/$HOME/.linuxbrew/opt/nvm/nvm.sh" 
+
 nvm install v8.12.0
 nvm use v8.12.0
 nvm install-latest-npm
+
+
+# Install npm packages
+
 npm install -g jscpd
 npm install -g gitmoji-cli
 npm install -g git-recent
 npm install -g git-stats
-
 npm install -g diff-so-fancy
+
+
+# Set git configuration
+
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global push.default current
-
-git alias conflicted "!nvim +Conflicted"
+git alias conflicted "nvim +Conflicted"
 git alias staged "diff --cached --name-only"
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | zsh
-export PATH="$HOME/.pyenv/bin:$PATH"
+# Install python
+
+ZLIB_ROOT_PATH=$(brew --cellar zlib)/$(brew list --versions zlib | tr ' ' '\n' | tail -1)
+ZLIB_INCLUDE_PATH=$ZLIB_ROOT_PATH/include
+ZLIB_LIBRARY_PATH=$ZLIB_ROOT_PATH/lib
+OPENSSL_INCLUDE_PATH=$(brew --prefix openssl)/include
+OPENSSL_LIBRARY_PATH=$(brew --prefix openssl)/lib
+SQLITE3_INCLUDE_PATH=$(brew --prefix sqlite3)/include
+SQLITE3_LIBRARY_PATH=$(brew --prefix sqlite3)/lib
+
+export CPPFLAGS="-I$ZLIB_INCLUDE_PATH -I$OPENSSL_INCLUDE_PATH -I$SQLITE3_INCLUDE_PATH"
+export LDFLAGS="-L$ZLIB_LIBRARY_PATH -L$OPENSSL_LIBRARY_PATH -L$SQLITE3_LIBRARY_PATH"
+pyenv install 3.6.5
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-PYTHON_CONFIGURE_OPTS='--enable-shared' pyenv install --skip-existing 3.6.5
 pyenv shell 3.6.5
 
-pip2 install pynvim
+
+# Install python packages
+
 pip3 install pynvim
-pip3 install cpplint git-plus termdown pygments
-pip3 install komodo-python3-dbgp pygments
+pip2 install pynvim
 
-# export CHEAT_PATH=$HOME/.config/cheat
-# pip3 install cheat cheat-ext
+pip3 install git-plus termdown pygments
 
-rm -rf $HOME/.oh-my-zsh
-curl -L http://install.ohmyz.sh | sh || true
-mkdir -p $HOME/.zsh
+
+# Install zsh packages
+
 curl -L git.io/antigen > $HOME/.zsh/antigen.zsh
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+# Install vim packages
+
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-if [ ! -d "$HOME/.zsh/vimman" ]; then
-  git clone https://github.com/yonchu/vimman.git $HOME/.zsh/vimman
-fi
 
-mkdir -p $HOME/.config/nvim/config
-mkdir -p $HOME/.config/vifm
-mkdir -p $HOME/.nvim/backup
-mkdir -p $HOME/.nvim/swap
-mkdir -p $HOME/.nvim/undodir
+nvim +PlugInstall +UpdateRemotePlugins +qall!
+
+
+# Copy configuration files
+
+mkdir -p $HOME/.zsh
+
+mkdir -p $HOME/.nvim/backup/
+mkdir -p $HOME/.nvim/swap/
+mkdir -p $HOME/.nvim/undodir/
 mkdir -p $HOME/.nvim/view/
 mkdir -p $HOME/.tmux
 
-cp configs/init.vim $HOME/.config/nvim/
-cp configs/lsp.vimrc $HOME/.config/nvim/config
-cp configs/snapshot.vim $HOME/.config/nvim/
-cp configs/vifmrc $HOME/.config/vifm/
-cp configs/.ackignore $HOME/.config/
-cp configs/.tmux.conf $HOME/
-cp configs/.zshrc $HOME/
-
-git clone https://github.com/sonph/onehalf
-chmod +x onehalf/gnome-terminal/onehalfdark.sh
-./onehalf/gnome-terminal/onehalfdark.sh
-rm -rf onehalf
-
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
-
-cat >> $HOME/.zshrc <<EOF
-
-export PATH="\$HOME/.pyenv/bin:\$PATH"
-eval "\$(pyenv init -)"
-eval "\$(pyenv virtualenv-init -)"
-EOF
-
-mkdir -p $HOME/.config/systemd/user
-cat > $HOME/.config/systemd/user/rdm.socket <<EOF
-[Unit]
-Description=RTags daemon socket
-[Socket]
-ListenStream=%t/rdm.socket
-[Install]
-WantedBy=default.target
-EOF
-
-cat > $HOME/.config/systemd/user/rdm.service <<EOF
-[Unit]
-Description=RTags daemon
-Requires=rdm.socket
-[Service]
-Type=simple
-ExecStart=`which rdm` -v --inactivity-timeout 300 --log-flush
-ExecStartPost=/bin/sh -c "echo +19 > /proc/\$MAINPID/autogroup"
-Nice=19
-CPUSchedulingPolicy=idle
-EOF
-systemctl --user enable rdm.socket
-systemctl --user start rdm.socket
-
-nvim +PlugInstall +UpdateRemotePlugins +qall! # nvim -S $HOME/.config/nvim/snapshot.vim
-
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-fi
-
-rm -rf ~/.config/vifm/colors
-git clone https://github.com/vifm/vifm-colors ~/.config/vifm/colors
-
-zsh --rcs $HOME/.zshrc || true
-tmux source-file ~/.tmux.conf
+install -m 644 -D configs/init.vim -t $HOME/.config/nvim/
+install -m 644 -D configs/snapshot.vim -t $HOME/.config/nvim/
+install -m 644 -D configs/lsp.vimrc -t $HOME/.config/nvim/config/
+install -m 644 -D configs/.ackignore -t $HOME/
+install -m 644 -D configs/.tmux.conf -t $HOME/
+install -m 644 -D configs/.zshrc -t $HOME/
