12ccca0395717ed0a902863e2237d315bea0de64
rusdevops <rusdevops@gmail.com>
2019-10-27 17:24:32 +0000

    updated dockerfile for linuxbrew

--- Dockerfile
+++ Dockerfile
@@ -1,52 +1,11 @@
-FROM rusdevops/bootstrap-cpp
+FROM ubuntu:19.04
 
-RUN add-apt-repository -y ppa:neovim-ppa/stable && apt update
-RUN apt -y install curl git git-extras zsh astyle python3-dev \
-    neovim silversearcher-ag python3-pip python-pip \
-    tmux exuberant-ctags valgrind gdb && \
-    rm -rf /var/lib/apt/lists/*
-
-RUN git clone git://github.com/rkitover/vimpager && \
-    cd vimpager && make install && cd .. && rm -rf vimpager
-
-RUN pip3 install komodo-python3-dbgp pynvim && pip2 install pynvim
-
-ENV TERM xterm-256color
-
-RUN curl -L http://install.ohmyz.sh | sh || true
-
-COPY .zshrc /tmp/
-COPY .tmux.conf /tmp/
-COPY init.vim /tmp/
+RUN apt update -y && \
+    apt upgrade -y && \
+    apt install -y build-essential curl file git
 
 RUN useradd -m developer
-RUN chown developer /tmp/.tmux.conf
-RUN chown developer /tmp/.zshrc
-RUN chown developer /tmp/init.vim
-
-RUN git clone https://github.com/facebook/PathPicker.git /usr/local/PathPicker && \
-    ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
-
 USER developer
 
-RUN mkdir $HOME/.zsh
-RUN curl -L git.io/antigen > $HOME/.zsh/antigen.zsh
-RUN curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-RUN mv /tmp/.zshrc $HOME/
-RUN mkdir -p $HOME/.config/nvim
-RUN mv /tmp/init.vim $HOME/.config/nvim/
-RUN mv /tmp/.tmux.conf $HOME/
-
-RUN mkdir -p $HOME/.nvim/backup
-RUN mkdir -p $HOME/.nvim/swap
-RUN nvim +PlugInstall +qall
-
-RUN git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-RUN git clone https://github.com/yonchu/vimman.git $HOME/.zsh/vimman
-RUN zsh --rcs $HOME/.zshrc || true
-
-ENV SHELL /usr/bin/zsh
-
-ENTRYPOINT zsh
+COPY bootstrap.sh /tmp/
+RUN /tmp/bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,44 +9,39 @@ git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
 mkdir ~/.linuxbrew/bin
 ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
 eval $(~/.linuxbrew/bin/brew shellenv)
-~/.linuxbrew/bin/brew shellenv
+
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
 # ----------------------------------------------------------------------------
 # Install brew packages
 # ----------------------------------------------------------------------------
 
-brew install zsh
-brew install pyenv
-brew install nvm
-
-brew install neovim
-brew install tmux
-
-brew install fpp
-brew install ag
-brew install fzf
-brew install ctags
-
-brew install git-extras
-brew install tig
-brew install highlight
-
-brew install htop
-brew install catimg
-
-
-brew install grc
-brew install jid
-
-brew install httpie
-brew install ncdu
+brew install zsh \
+             pyenv \
+             nvm \
+             neovim \
+             tmux \
+             fpp \
+             ag \
+             fzf \
+             ctags \
+             git-extras \
+             tig \
+             highlight \
+             htop \
+             catimg \
+             grc \
+             jid \
+             httpie \
+             ncdu
 
 # ----------------------------------------------------------------------------
 # Install nodejs
 # ----------------------------------------------------------------------------
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$HOME/.linuxbrew/opt/nvm/nvm.sh" ] && . "/$HOME/.linuxbrew/opt/nvm/nvm.sh" 
+[ -s "$HOME/.linuxbrew/opt/nvm/nvm.sh" ] && . "/$HOME/.linuxbrew/opt/nvm/nvm.sh"
 
 nvm install v8.12.0
 nvm use v8.12.0
@@ -56,12 +51,12 @@ nvm install-latest-npm
 # Install npm packages
 # ----------------------------------------------------------------------------
 
-npm install -g jscpd
-npm install -g gitmoji-cli
-npm install -g git-recent
-npm install -g git-stats
-npm install -g diff-so-fancy
-npm install -g vtop
+npm install -g jscpd \
+               gitmoji-cli \
+               git-recent \
+               git-stats \
+               diff-so-fancy \
+               vtop
 
 # ----------------------------------------------------------------------------
 # Set git configuration
@@ -69,6 +64,7 @@ npm install -g vtop
 
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global push.default current
+
 git alias conflicted "nvim +Conflicted"
 git alias staged "diff --cached --name-only"
 
@@ -76,17 +72,18 @@ git alias staged "diff --cached --name-only"
 # Install python packages
 # ----------------------------------------------------------------------------
 
-pip3 install pynvim
 pip2 install pynvim
-
-pip3 install git-plus termdown pygments
+pip3 install pynvim \
+             git-plus
 
 # ----------------------------------------------------------------------------
 # Install package managers
 # ----------------------------------------------------------------------------
 
 curl -L git.io/antigen -o $HOME/.zsh/antigen.zsh --create-dirs
+
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -94,23 +91,25 @@ curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 # Copy configuration files
 # ----------------------------------------------------------------------------
 
-mkdir -p $HOME/.zsh
+install -m 644 -D configs/.tmux.conf -t $HOME/
+install -m 644 -D configs/.zshrc -t $HOME/
+
+install -m 644 -D configs/init.vim -t $HOME/.config/nvim/
+install -m 644 -D configs/lsp.vimrc -t $HOME/.config/nvim/config/
+
+install -m 644 -D configs/.ackignore -t $HOME/
+
+# ----------------------------------------------------------------------------
+# Create neovim directories
+# ----------------------------------------------------------------------------
 
 mkdir -p $HOME/.nvim/backup/
 mkdir -p $HOME/.nvim/swap/
 mkdir -p $HOME/.nvim/undodir/
 mkdir -p $HOME/.nvim/view/
-mkdir -p $HOME/.tmux
-
-install -m 644 -D configs/init.vim -t $HOME/.config/nvim/
-install -m 644 -D configs/snapshot.vim -t $HOME/.config/nvim/
-install -m 644 -D configs/lsp.vimrc -t $HOME/.config/nvim/config/
-install -m 644 -D configs/.ackignore -t $HOME/
-install -m 644 -D configs/.tmux.conf -t $HOME/
-install -m 644 -D configs/.zshrc -t $HOME/
 
 # ----------------------------------------------------------------------------
-# Install vim packages
+# Install neovim packages
 # ----------------------------------------------------------------------------
 
 nvim +PlugInstall +UpdateRemotePlugins +qall!
