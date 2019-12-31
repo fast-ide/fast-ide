8cf75733fa75288456fdd04d7cf8222b426f8563
rusdevops <rusdevops@gmail.com>
2019-12-31 20:04:16 +0300

    added building for centos, debian, fedora and ubuntu

--- scripts/linuxbrew/debian/requirements.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-apt update -y
-apt upgrade -y
-apt install -y build-essential curl file git xclip
--- scripts/linuxbrew/redhat/requirements.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-yum groupinstall 'Development Tools'
-yum install curl file git
-yum install libxcrypt-compat # needed by Fedora 30 and up
