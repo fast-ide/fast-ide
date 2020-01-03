7a0a5bcafe602883d81e9d26323762dafe15f49a
rusdevops <rusdevops@gmail.com>
2020-01-03 21:12:15 +0300

    removed unnecessary files

--- scripts/docker_build.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-pushd linuxbrew/docker/${OS_FAMILY}-${OS_VERSION}
-tar -czh . | docker build -t linuxbrew/${OS_FAMILY}:${OS_VERSION} -
-popd
-cd docker/${OS_FAMILY}-${OS_VERSION}
-tar -czh . | docker build -t fastide/${OS_FAMILY}:${OS_VERSION} -
--- scripts/docker_deploy.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push fastide/${OS_FAMILY}:${OS_VERSION}
-
