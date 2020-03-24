19e3df99e42bbf3bbc291a39e2599a1ac55bbe7f
rusdevops <rusdevops@gmail.com>
2020-03-24 22:04:49 +0300

    removed installation of vim plugins

--- install.sh
+++ install.sh
@@ -16,5 +16,4 @@ cp configs/snapshot.vim $HOME/.config/nvim/
 cp configs/lsp.vim $HOME/.config/nvim/
 
 zsh -c "source $HOME/.zshrc"
-nvim -S $HOME/.config/nvim/snapshot.vim <<EOF
-EOF
+# nvim -S $HOME/.config/nvim/snapshot.vim
