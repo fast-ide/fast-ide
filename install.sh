1901ca4c832b30be39803275fa5d6e8e960e6732
rusdevops <rusdevops@gmail.com>
2020-03-24 23:06:51 +0300

    Revert "disabled installation of zsh plugins"
    This reverts commit 44262118044d25933bcea6bb70b442620c0144ad.

--- install.sh
+++ install.sh
@@ -15,5 +15,5 @@ cp configs/init.vim $HOME/.vimrc
 cp configs/snapshot.vim $HOME/.config/nvim/
 cp configs/lsp.vim $HOME/.config/nvim/
 
-# zsh -c "source $HOME/.zshrc"
+zsh -c "source $HOME/.zshrc"
 vim -T dumb -S $HOME/.config/nvim/snapshot.vim +qa
