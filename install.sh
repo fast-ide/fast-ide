7226449ec43f68e448b03531b927f374c1aa657c
rusdevops <rusdevops@gmail.com>
2020-09-29 14:59:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ cp configs/.tmux.conf $HOME/
 cp configs/.zshrc $HOME/
 
 mkdir -p $HOME/.config/nvim/
+mkdir -p $HOME/.config/lf/
 
 cp configs/init.vim $HOME/.config/nvim/
 cp configs/snapshot.vim $HOME/.config/nvim/
 cp configs/lsp.vim $HOME/.config/nvim/
+cp configs/lfrc $HOME/.config/lf/lfrc
 
 tmux start-server && tmux new-session -d && sleep 30
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
