50f23c91749182448b566c7848fe758dd28efff3
rusdevops <rusdevops@gmail.com>
2020-10-19 22:51:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,4 +25,4 @@ zsh -c "source $HOME/.zshrc"
 nvim -S $HOME/.config/nvim/snapshot.vim +qa --headless
 
 # will be fixed in https://github.com/fast-ide/fast-ide/issues/55
-# nvim -c "CocInstall coc-clangd coc-python coc-go" +qa --headless
+# nvim -c "CocInstall coc-clangd coc-python coc-go coc-tsserver" +qa --headless
