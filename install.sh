973984f4559d809bf96f2d2a4f7f389c41715f86
rusdevops <rusdevops@gmail.com>
2020-10-23 12:31:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,4 +24,4 @@ tmux kill-server || echo "already killed"
 zsh -c "source $HOME/.zshrc" || echo "warning: one of antigen packages is worth fixing"
 nvim -S $HOME/.config/nvim/snapshot.vim +qa --headless
 
-# nvim -c "CocInstall coc-clangd coc-python coc-go coc-tsserver" +qa --headless
+nvim -c "CocInstall coc-clangd coc-python coc-go coc-tsserver" +qa --headless
