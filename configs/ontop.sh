5506c178f965f5f1dbf99818904e2a245926047c
rusdevops <rusdevops@gmail.com>
2021-04-29 09:38:08 +0300

    Create ontop.sh

---
+++ configs/ontop.sh
@@ -0,0 +1,46 @@
+if [ "x$ONTOP" = "xfalse" ]; then
+else
+  ONTOP=true
+fi
+
+if [ "$ONTOP_COLOR" = "" ]; then
+  ONTOP_COLOR="green"
+fi
+
+function _accept_line_ontop {
+  if [ "x$ONTOP" = "xtrue" ]; then
+    tput cup 0 0
+    zle redisplay
+  fi
+  zle .accept-line
+}
+
+zle -N accept-line _accept_line_ontop
+
+function ontop
+{
+  local arg=$1
+  if [ "x$arg" = "xon" ]; then
+    ONTOP=true
+  fi
+
+  if [ "x$arg" = "xoff" ]; then
+    ONTOP=false
+  fi
+
+  if [ "x$arg" = "xtoggle" ]; then
+    ontop_toggle
+  fi
+}
+
+function ontop_toggle
+{
+  if [ "x$ONTOP" = "xtrue" ]; then
+    ontop off
+  else
+    ontop on
+  fi
+}
+
+zle -N ontop_toggle ontop_toggle
+bindkey "^X^L" ontop_toggle
